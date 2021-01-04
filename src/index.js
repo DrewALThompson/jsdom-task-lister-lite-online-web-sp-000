77f99333b73a50f739dc73b48f4589e28542b758
Drew Thompson <drewallynthompson@gmail.com>
2021-01-04 08:21:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     
-    let input = (document.getElementById('new-task-description')).value;
+    let input = document.getElementById('new-task-description');
+    let inputValue = input.value
     
     let liItem = document.createElement('li');
     
