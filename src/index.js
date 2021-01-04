fd155c59017adaa516855ebd010561546f794e78
Drew Thompson <drewallynthompson@gmail.com>
2021-01-04 08:23:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     
-    let input = document.getElementById('new-task-description');
-    let inputValue = input.value;
+    let input = document.getElementById('new-task-description').value;
     
     let liItem = document.createElement('li');
     
     let ulAll = document.querySelector('ul');
     if (input.value != '') {
-      liItem.innerText = input.value;
+      liItem.innerText = input;
       ulAll.append(liItem);
-      input.value = '';
+      input = '';
     };
     
   })
