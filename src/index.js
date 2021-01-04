2747a948cd5d5bfcba11f019e50931c0da916033
Drew Thompson <drewallynthompson@gmail.com>
2021-01-04 08:20:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ document.addEventListener("DOMContentLoaded", () => {
       liItem.innerText = input;
       ulAll.append(liItem);
       input.value = '';
-      return input;
     };
     
   })
