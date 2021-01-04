fda35f7e9e3fec594bf5e9743f35c940565d37aa
Drew Thompson <drewallynthompson@gmail.com>
2021-01-04 08:09:37 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     
-    let input = document. 
+    let input = document.getElementById('') 
   })
 });
